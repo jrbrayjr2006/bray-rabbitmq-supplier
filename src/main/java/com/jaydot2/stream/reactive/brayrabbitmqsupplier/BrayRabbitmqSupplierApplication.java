2cb17e60cfe439d95092edf55906e11d79e9310a
James Bray <jrbrayjr2006@gmail.com>
2021-09-26 18:24:19 -0500

    Initial commit

---
+++ src/main/java/com/jaydot2/stream/reactive/brayrabbitmqsupplier/BrayRabbitmqSupplierApplication.java
@@ -0,0 +1,13 @@
+package com.jaydot2.stream.reactive.brayrabbitmqsupplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrayRabbitmqSupplierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrayRabbitmqSupplierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaydot2/stream/reactive/brayrabbitmqsupplier/BrayRabbitmqSupplierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaydot2.stream.reactive.brayrabbitmqsupplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrayRabbitmqSupplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
